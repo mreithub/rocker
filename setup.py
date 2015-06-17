f3c472d4b7374913ea208b4fd8576d96098e5f10
Manuel Reithuber <manuel@reithuber.net>
2015-06-17 14:41:34 +0200

    fixed setup.py's license classifier

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache License',
+        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
