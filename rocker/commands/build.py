2953d2e3993058866abb776b2604ca78e1d366ba
Manuel Reithuber <git@manuel.reithuber.net>
2015-10-21 20:17:05 +0200

    fixed issue in build command (wrong indentation)

--- rocker/commands/build.py
+++ rocker/commands/build.py
@@ -7,4 +7,4 @@ Builds the docker image in the specified subdir"""
 def run(args, r):
 	if len(args) != 2:
 		help.usage(None, "'build' expects exactly one argument (the image path)")
-		image.build(args[1], rocker=r)
+	image.build(args[1], rocker=r)
